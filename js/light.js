374ed6e45be8a6adc6ee85a9c3f68be0345897ca
lune-cache <freyjassu@outlook.com>
2023-02-15 22:04:34 +0800

    Site updated: 2023-02-15 22:04:29

--- js/light.js
+++ js/light.js
@@ -1,6 +1,6 @@
 // 霓虹灯效果
 // 颜色数组
-var arr = ["#39c5bb", "#f14747", "#f1a247", "#f1ee47", "#b347f1", "#1edbff", "#ed709b", "#5636ed"];
+var arr = ["#f1ee47", "#ffffff"];
 // 颜色索引
 var idx = 0;
 
@@ -23,7 +23,7 @@ function changeColor() {
 
         }
         idx++;
-        if (idx == 8) {
+        if (idx == 2) {
             idx = 0;
         }
     } else {
